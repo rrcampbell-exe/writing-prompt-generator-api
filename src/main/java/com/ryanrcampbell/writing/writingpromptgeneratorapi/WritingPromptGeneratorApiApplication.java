e432022ad3f4bfca0446a3f15a429e64cd611d1c
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-12-30 05:11:59 -0600

    feat: initialize project

---
+++ src/main/java/com/ryanrcampbell/writing/writingpromptgeneratorapi/WritingPromptGeneratorApiApplication.java
@@ -0,0 +1,13 @@
+package com.ryanrcampbell.writing.writingpromptgeneratorapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WritingPromptGeneratorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WritingPromptGeneratorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ryanrcampbell/writing/writingpromptgeneratorapi/WritingPromptGeneratorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ryanrcampbell.writing.writingpromptgeneratorapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WritingPromptGeneratorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
