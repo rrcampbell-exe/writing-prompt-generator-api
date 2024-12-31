dc3beb7fba19ee4a3cf8d74d91831b9e7e5b5550
rrcampbell-exe <campbell.ryan.r@gmail.com>
2024-12-31 10:12:26 -0600

    fix: limit endpoint access

--- src/main/java/com/ryanrcampbell/writing/writingpromptgeneratorapi/controller/PromptController.java
+++ src/main/java/com/ryanrcampbell/writing/writingpromptgeneratorapi/controller/PromptController.java
@@ -2,10 +2,12 @@ package com.ryanrcampbell.writing.writingpromptgeneratorapi.controller;
 
 import com.ryanrcampbell.writing.writingpromptgeneratorapi.service.PromptService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = {"http://ryanrcampbell.com", "https://ryanrcampbell.com", "http://localhost:5173", "http://localhost:8080"})
 public class PromptController {
     private final PromptService promptService;
 
