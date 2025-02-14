09b991511367409182c43f81fc47503d78ae5287
rrcampbell-exe <campbell.ryan.r@gmail.com>
2025-02-14 06:14:15 -0600

    fix: update error message

--- middleware/rateLimit.ts
+++ middleware/rateLimit.ts
@@ -5,7 +5,7 @@ const rateLimiter: RateLimitRequestHandler = rateLimit({
   windowMs: 3 * 60 * 60 * 1000, // 3 hours
   max: 10, // limit each IP to 10 requests per windowMs
   message: {
-    error: "Rate limit reached. Please try again later."
+    error: "In a world where a writer pings the Writing Prompt Generator too frequently, they never actually get to writing. Please try again in a few hours. 😊"
   }
 })
 
